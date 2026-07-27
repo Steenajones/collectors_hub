5ae84b5de3fdac65ef73fe8d6bbbf5eea736fc27
Steenajones <fruityjones7@gmail.com>
2026-07-27 17:49:29 +0530

    Remove HashRouter basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="/collectors_hub">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
